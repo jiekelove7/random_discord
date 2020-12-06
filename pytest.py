f99e0ddb80a5529983a4150b81fa4d5fb96268f8
滑板鞋 <aijm73@gmail.com>
2020-12-07 03:23:56 +0800

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,17 @@
+import discord
+import config
+
+class MyClient(discord.Client):
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        await message.channel.send('test')
+
+        # Prints message it sees
+        # Success!
+        # print('{0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(config.token)
